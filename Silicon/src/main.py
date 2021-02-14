6a9a65ae67d3f8ed6e080d54e85e65b06c533c72
Your Name <louis.dalibard@gmail.com>
2021-02-14 20:14:30 +0100

    Update main.py

--- Silicon/src/main.py
+++ Silicon/src/main.py
@@ -4,17 +4,28 @@ import board
 import busio
 import time
 
-
+# User variables
+version = "1.01"
+addresses = [0x00,0x01,0x02,0x03,0x04]
 
 # Initialise program.
 # Create the I2C interface.
+print("Starting I²C interface... ", end="")
 i2c = busio.I2C(board.SCL, board.SDA)
+print("Done.")
 # Initialise displays.
-tm_a = segments.Seg14x4(i2c, address=0x00) # Address 0
-tm_b = segments.Seg14x4(i2c, address=0x01) # Address 1
-tm_c = segments.Seg14x4(i2c, address=0x02) # Address 2
-tm_d = segments.Seg14x4(i2c, address=0x03) # Address 3
-tm_e = segments.Seg14x4(i2c, address=0x04) # Address 4
+print("Initialising displays: ", end="")
+print("1 ", end=""))
+tm_a = segments.Seg14x4(i2c, address=addresses[0]) # Address 0
+print("2 ", end=""))
+tm_b = segments.Seg14x4(i2c, address=addresses[1]) # Address 1
+print("3 ", end=""))
+tm_c = segments.Seg14x4(i2c, address=addresses[2]) # Address 2
+print("4 ", end=""))
+tm_d = segments.Seg14x4(i2c, address=addresses[3]) # Address 3
+print("5 ", end=""))
+tm_e = segments.Seg14x4(i2c, address=addresses[4]) # Address 4
+print("Done.")
 
 # Define functions.
 # Define "split text between displays" function.
@@ -34,11 +45,18 @@ def updatebtc(price, currency):
     printodisplays("Bitcoin: "+pricestring+currency)
 
 # Display loading text. 
+print("Displaying startup text...")
 printodisplays("[Open7SClock, xE0F9]")
 time.sleep(1)
+printodisplays("[[  Silicon v"+version+"  ]]")
+time.sleep(1)
 
+
+oldseconds = 99
 # Start main program loop
 while 1:
     now = datetime.datetime.now()
-    updatetime(now)
+	if now.seconds != oldseconds:
+		updatetime(now)
+		oldseconds = now.seconds
     time.sleep(0.2)
