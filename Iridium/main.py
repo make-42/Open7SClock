74918223651d27503b1885dbc08fc2add6aad50e
Louis Dalibard <louis.dalibard@gmail.com>
2021-06-23 10:00:59 +0200

    Update main.py

--- Iridium/main.py
+++ Iridium/main.py
@@ -7,23 +7,25 @@ import cherrypy
 import json
 
 
-player = "rhythmbox"#"plasma-browser-integration"
+defplayer = "rhythmbox"#"plasma-browser-integration"
 
 def runcommand(command):
     return subprocess.Popen(command, shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8")
-
 class StringGenerator(object):
     @cherrypy.expose
     @cherrypy.tools.json_out()
     def index(self):
-        status = runcommand("playerctl status -p "+player)
-        volume = runcommand("playerctl volume -p "+player)
-        album = runcommand("playerctl metadata xesam:album -p "+player)
-        artist = runcommand("playerctl metadata xesam:artist -p "+player)
-        title = runcommand("playerctl metadata xesam:title -p "+player)
-        duration = float(runcommand("playerctl metadata mpris:length -p "+player))/1000000
-        position = float(runcommand("playerctl position -p "+player))
-
+        status = runcommand("playerctl status")
+        volume = runcommand("playerctl volume")
+        album = runcommand("playerctl metadata xesam:album")
+        artist = runcommand("playerctl metadata xesam:artist")
+        title = runcommand("playerctl metadata xesam:title")
+        try:
+            duration = float(runcommand("playerctl metadata mpris:length"))/1000000
+            position = float(runcommand("playerctl position"))
+        except:
+            duration = 0
+            position = 0
         payload = {"status":status,"volume":volume,"album":album,"artist":artist,"title":title,"duration":duration,"position":position}
         return json.dumps(payload)
 
